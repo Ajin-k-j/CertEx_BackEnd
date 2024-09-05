251d5dc3fc4a4a2d174896d1cfb49ea44896cb29
sahirnisar <sahirnisar4388@gmail.com>
2024-09-05 09:42:33 +0530

    Test case added

---
+++ CertEx_Test/Controllers/AwsBarGraphControllerTest.cs
@@ -0,0 +1,110 @@
+﻿using CertExBackend.Controllers;
+using CertExBackend.DTOs;
+using CertExBackend.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CertEx_Test.Controllers
+{
+    [TestFixture]
+    public class AwsBarGraphControllerTest
+    {
+        private Mock<IAwsBarGraphService> _mockAwsBarGraphService;
+        private AwsBarGraphController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockAwsBarGraphService = new Mock<IAwsBarGraphService>();
+            _controller = new AwsBarGraphController(_mockAwsBarGraphService.Object);
+        }
+
+        [Test]
+        public async Task GetAll_NoData_ReturnsOkWithEmptyList()
+        {
+            // Arrange
+            _mockAwsBarGraphService.Setup(service => service.GetAwsBarGraphDataAsync())
+                                   .ReturnsAsync(new List<AwsBarGraphDto>());
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<IEnumerable<AwsBarGraphDto>>());
+            Assert.That(okResult.Value as IEnumerable<AwsBarGraphDto>, Is.Empty);
+        }
+
+        [Test]
+        public async Task GetAll_WithData_ReturnsOkWithData()
+        {
+            // Arrange
+            var barGraphData = new List<AwsBarGraphDto>
+            {
+                new AwsBarGraphDto { ExamStatus = "Passed", ExamDate = new DateTime(2024, 1, 15), DepartmentName = "IT" },
+                new AwsBarGraphDto { ExamStatus = "Failed", ExamDate = new DateTime(2024, 2, 20), DepartmentName = "HR" }
+            };
+            _mockAwsBarGraphService.Setup(service => service.GetAwsBarGraphDataAsync())
+                                   .ReturnsAsync(barGraphData);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<IEnumerable<AwsBarGraphDto>>());
+            Assert.That(okResult.Value, Is.EqualTo(barGraphData));
+        }
+
+        [Test]
+        public async Task GetFiltered_ValidRequest_ReturnsOkWithFilteredData()
+        {
+            // Arrange
+            var filteredData = new MonthlyExamCompletionDTO
+            {
+                January = 5,
+                February = 8
+            };
+            _mockAwsBarGraphService.Setup(service => service.GetFilteredExamCompletionDataAsync(2024, 1))
+                                   .ReturnsAsync(filteredData);
+
+            // Act
+            var result = await _controller.GetFiltered(2024, 1);
+
+            // Assert
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<MonthlyExamCompletionDTO>());
+            Assert.That(okResult.Value, Is.EqualTo(filteredData));
+        }
+
+        [Test]
+        public async Task GetFiltered_WithNullDepartmentId_ReturnsOkWithFilteredData()
+        {
+            // Arrange
+            var filteredData = new MonthlyExamCompletionDTO
+            {
+                March = 12,
+                April = 7
+            };
+            _mockAwsBarGraphService.Setup(service => service.GetFilteredExamCompletionDataAsync(2024, null))
+                                   .ReturnsAsync(filteredData);
+
+            // Act
+            var result = await _controller.GetFiltered(2024, null);
+
+            // Assert
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<MonthlyExamCompletionDTO>());
+            Assert.That(okResult.Value, Is.EqualTo(filteredData));
+        }
+    }
+}
---
+++ CertEx_Test/Controllers/UserPendingActionControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using CertExBackend.Controllers;
+using CertExBackend.DTOs;
+using CertExBackend.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CertEx_Test.Controllers
+{
+    [TestFixture]
+    public class UserPendingActionControllerTest
+    {
+        private Mock<IUserPendingActionService> _mockUserPendingActionService;
+        private UserPendingActionController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockUserPendingActionService = new Mock<IUserPendingActionService>();
+            _controller = new UserPendingActionController(_mockUserPendingActionService.Object);
+        }
+
+        [Test]
+        public async Task GetUserPendingActions_ReturnsOkWithData()
+        {
+            // Arrange
+            var pendingActions = new List<UserPendingActionDto>
+            {
+                new UserPendingActionDto
+                {
+                    Id = 1,
+                    EmployeeId = 101,
+                    CertificationId = 201,
+                    NominationStatusFromNomination = "Pending",
+                    NominationStatusFromCertificationExam = "Approved",
+                    CertificationName = "Certification A"
+                },
+                new UserPendingActionDto
+                {
+                    Id = 2,
+                    EmployeeId = 102,
+                    CertificationId = 202,
+                    NominationStatusFromNomination = "Completed",
+                    NominationStatusFromCertificationExam = "Pending",
+                    CertificationName = "Certification B"
+                }
+            };
+
+            _mockUserPendingActionService.Setup(service => service.GetUserPendingActionsAsync())
+                                         .ReturnsAsync(pendingActions);
+
+            // Act
+            var result = await _controller.GetUserPendingActions();
+
+            // Assert
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+            var okResult = result.Result as OkObjectResult;
+            Assert.That(okResult.Value, Is.InstanceOf<IEnumerable<UserPendingActionDto>>());
+            Assert.That(okResult.Value as IEnumerable<UserPendingActionDto>, Is.EqualTo(pendingActions));
+        }
+    }
+}
